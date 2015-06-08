ddd540251da6ff65011893d61b15c6e10c875431
Shakna Israel <s4b3r6@gmail.com>
2015-06-08 12:47:00 +0000

    Fixed count-words loading word list

--- count-words.py
+++ count-words.py
@@ -31,6 +31,25 @@ def wordIgnoreList(cliArgs):
         ignoredWordsList = ["the","be","to","of","and","a","in","that","have","I","it","for","not","on","with","he","as","you","do","at","this","but","his","by","from","they","we","say","her","she","or","an","will","my","one","all","would","there","their","what","so","up","out","if","about","who","get","which","go","me","when","make","can","like","time","no","just","him","know","take","people","into","year","your","good","some","could","them","see","other","than","then","now","look","only","come","its","over","think","also","back","after","use","two","how","our","work","first","well","way","even","new","want","because","any","these","give","day","most","us"]
     return ignoredWordsList
 
+def fileList(fileInput):
+    openFile = open(fileInput, 'r')
+    # Initiate the word list that we'll be building from the input file.
+    wordList = []
+    # Iterate through the file we opened earlier.
+    for line in openFile:
+    # Let's split each word apart into a list.
+        words=line.split()
+        for word in words:
+            word = word.lower()
+            wordList.append(word)
+    # Close the file once everything is in memory.
+    openFile.close()
+    # Remove all duplicate words.
+    wordList = list(set(wordList))
+    # Alphabetise the list naively.
+    wordList.sort()
+    return wordList
+
 def buildDict(cliArgs, characterIgnoreList, wordedIgnoreList):
     # Initialise dict
     dictList = {}

c15f70e5a1512ef227ee3a23fb0f63a2872857e1
Shakna Israel <s4b3r6@gmail.com>
2015-06-08 11:15:16 +0000

    Allowed users to specify their own list files

--- common-words.py
+++ common-words.py
@@ -3,49 +3,88 @@
 # Argparse will handle the commandline for us.
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input", help="The input file. Normally a prose document.")
-parser.add_argument("-o", "--output", help="The output file name, without file extension.")
-cliArgs = parser.parse_args()
-
-# Open the file the user asked us to.
-openFile = open(cliArgs.input, 'r')
-# Initiate the word list that we'll be building from the input file.
-wordList = []
+def init_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="The input file. Normally a prose document.")
+    parser.add_argument("-o", "--output", help="The output file name, without file extension.")
+    parser.add_argument("-wl", "--word-list", help="Define your own list of words to ignore. Seperate with spaces.")
+    parser.add_argument("-cl", "--character-list", help="Define your own list of formatting characters to ignore. Seperate with spaces.")
+    cliArgs = parser.parse_args()
+    return cliArgs
 
 # Escape characters that might occur that we don't care about.
 # Current Issue: Doesn't ignore the '#' symbol, because escaping it is a pain in the butt in Python.
-characterIgnoreList = ['"','.',',',';',':','(',')','{','}','[',']','/','\\','!','-','_','+','=','*','&','^','%','$','@']
+def charIgnoreList(cliArgs):
+    if cliArgs.character_list:
+        characterIgnoreList = fileList(cliArgs.character_list)
+    else:
+        characterIgnoreList = ['"','.',',',';',':','(',')','{','}','[',']','/','\\','!','-','_','+','=','*','&','^','%','$','@']
+    return characterIgnoreList
 
 # A list of common English words to ignore.
-ignoredWordsList = ["be","to","of","and","a","in","that","have","I","it","for","not","on","with","he","as","you","do","at","this","but","his","by","from","they","we","say","her","she","or","an","will","my","one","all","would","there","their","what","so","up","out","if","about","who","get","which","go","me","when","make","can","like","time","no","just","him","know","take","people","into","year","your","good","some","could","them","see","other","than","then","now","look","only","come","its","over","think","also","back","after","use","two","how","our","work","first","well","way","even","new","want","because","any","these","give","day","most","us"]
+def wordIgnoreList(cliArgs):
+    if cliArgs.word_list:
+        ignoredWordsList = fileList(cliArgs.word_list)
+    else:
+        ignoredWordsList = ["be","to","of","and","a","in","that","have","I","it","for","not","on","with","he","as","you","do","at","this","but","his","by","from","they","we","say","her","she","or","an","will","my","one","all","would","there","their","what","so","up","out","if","about","who","get","which","go","me","when","make","can","like","time","no","just","him","know","take","people","into","year","your","good","some","could","them","see","other","than","then","now","look","only","come","its","over","think","also","back","after","use","two","how","our","work","first","well","way","even","new","want","because","any","these","give","day","most","us"]
+    return ignoredWordsList
 
-# Iterate through the file we opened earlier.
-for line in openFile:
+def fileList(fileInput):
+    openFile = open(fileInput, 'r')
+    # Initiate the word list that we'll be building from the input file.
+    wordList = []
+    # Iterate through the file we opened earlier.
+    for line in openFile:
     # Let's split each word apart into a list.
-    words=line.split()
-    # Let's iterate through this new list.
-    for word in words:
-        # Use our ignore list to get rid of formatting characters.
-        for char in characterIgnoreList:
-            word = word.replace(char,"")
-        word = unicode(word, 'ascii', 'ignore')
-        wordList.append(word)
+        words=line.split()
+        for word in words:
+            wordList.append(word)
+    # Close the file once everything is in memory.
+    openFile.close()
+    # Remove all duplicate words.
+    wordList = list(set(wordList))
+    # Alphabetise the list naively.
+    wordList.sort()
+    return wordList
 
-# Remove all duplicate words.
-wordList = list(set(wordList))
+def outputList(cliArgs, finalWordList):
+    # Alphabetise our final list.
+    finalWordList.sort()
+    # Print to the console, or an output file.
+    if cliArgs.output:
+        outFile = open(cliArgs.output, 'w+')
+        for word in finalWordList:
+            outFile.write(word + "\n")
+        outFile.close()
+    else:
+        for word in finalWordList:
+            print(word)
 
-# Remove all words using our ignore list as a basis.
-finalWordList = list(set(wordList).difference(ignoredWordsList))
+def removeFormat(wordList, formatCharsList):
+    newWordList = []
+    for word in wordList:
+        for char in formatCharsList:
+            word = word.replace(char, "")
+            try:
+                word = unicode(word, 'ascii', 'ignore')
+            except TypeError:
+                pass
+        newWordList.append(word)
+    return newWordList
 
-# Alphabetise the list naively.
-finalWordList.sort()
+def removeCommon(wordList, wordIgnoreList):
+    wordList = list(set(wordList).difference(wordIgnoreList))
+    return wordList
 
-# Print to the console, or an output file.
-if cliArgs.output:
-    outFile = open(cliArgs.output, 'w+')
-    for word in finalWordList:
-        outFile.write(word + "\n")
-else:
-    for word in finalWordList:
-        print word
+def main():
+    cliArgs = init_parse()
+    characterIgnoreList = charIgnoreList(cliArgs)
+    wordedIgnoreList = wordIgnoreList(cliArgs)
+    basicWordList = fileList(cliArgs.input)
+    formattedWordList = removeFormat(basicWordList, characterIgnoreList)
+    removedCommonWordsList = removeCommon(formattedWordList, wordedIgnoreList)
+    finalWordList = removedCommonWordsList
+    outputList(cliArgs, finalWordList)
+
+if __name__ == '__main__':
+    main()
